7a046dbe4e2ce66c083eb2a94517532a663520c1
siva0704 <vaibhavabg1234@gmail.com>
2025-03-16 11:34:03 +0530

    k

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,7 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import path from "path";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
   plugins: [react()],
-  base: "/edu-hub-connector-21/", // Add this for GitHub Pages
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "src"), // Fix alias for "@/components/..."
-    },
-  },
+  base: '/edu-hub-connector-21/', // e.g., '/my-react-app/'
 });
